39e95e32c8f126e2201812954aabe904c5400014
Genercode <72575486+autocode2020@users.noreply.github.com>
2023-01-12 14:53:18 +0000

    Update GenerCodeServiceProvider.php

--- src/GenerCodeServiceProvider.php
+++ src/GenerCodeServiceProvider.php
@@ -11,16 +11,14 @@ class GenerCodeServiceProvider extends ServiceProvider {
     }
 
     public function boot() {
-        $domain = $_ENV['x-domain'];
-        config(["database.default" => $domain]);
-        config(["filesystems.default"=>"s3" . $domain]);
-        config(["mail.default"=>$domain]);
-        config(["document.default"=> $domain]);
-        config(["auth.providers.users.connection"=> $domain]);
-        config(["auth.passwords.users.connection"=> $domain]);
-        
-        $queue = config("queue.connections.sqs." . $domain . "_queue");
-        config(["queue.connections.sqs.queue"=> $queue]);
-        $this->app->instance("entity_factory", new \PressToJam\EntityFactory());
+        $app = app();
+    
+        $app->instance("entity_factory", new \PressToJam\EntityFactory());
+    
+        if ($app->runningInConsole()) {
+            $profile = $app->make(\PressToJam\Profile\AdminProfile::class);
+            $profile->id = 1;
+            $app->instance("profile", $profile);
+        }
     }
 }
