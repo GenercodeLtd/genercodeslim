514bebc79d26269170d7015168a39817f6898168
Genercode <72575486+autocode2020@users.noreply.github.com>
2023-01-18 18:14:56 +0000

    Update GenerCodeSlimBridge.php

--- src/GenerCodeSlimBridge.php
+++ src/GenerCodeSlimBridge.php
@@ -272,7 +272,7 @@ class GenerCodeSlimBridge extends \Illuminate\Foundation\Http\Kernel
 
             $group->get("/{name}", function($request, $response, $args) {
                 $report = $this->get(Controllers\ReportsController::class);
-                $data = $audit->get($args["name"], $request->getQueryParams());
+                $data = $report->get($args["name"], $request->getQueryParams());
                 $response->getBody()->write(json_encode($data));
                 return $response;
             });
@@ -280,7 +280,7 @@ class GenerCodeSlimBridge extends \Illuminate\Foundation\Http\Kernel
 
             $group->get("/{name}/{field}/{agg}", function($request, $response, $args) {
                 $report = $this->get(Controllers\ReportsController::class);
-                $data = $audit->getAggregate($args["name"], $args["field"], $args["agg"], $request->getQueryParams());
+                $data = $report->getAggregate($args["name"], $args["field"], $args["agg"], $request->getQueryParams());
                 $response->getBody()->write(json_encode($data));
                 return $response;
             });
